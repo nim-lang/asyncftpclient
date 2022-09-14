37d65878276fc6ed14844b042521e5f3c6f2d821
hlaaftana <10591326+metagn@users.noreply.github.com>
2022-09-14 04:50:31 +0300

    make into nimble package

--- src/asyncftpclient.nim
+++ src/asyncftpclient.nim
@@ -429,20 +429,3 @@ proc newAsyncFtpClient*(address: string, port = Port(21),
   result.progressInterval = progressInterval
   result.dsockConnected = false
   result.csock = newAsyncSocket()
-
-when not defined(testing) and isMainModule:
-  var ftp = newAsyncFtpClient("example.com", user = "test", pass = "test")
-  proc main(ftp: AsyncFtpClient) {.async.} =
-    await ftp.connect()
-    echo await ftp.pwd()
-    echo await ftp.listDirs()
-    await ftp.store("payload.jpg", "payload.jpg")
-    await ftp.retrFile("payload.jpg", "payload2.jpg")
-    await ftp.rename("payload.jpg", "payload_renamed.jpg")
-    await ftp.store("payload.jpg", "payload_remove.jpg")
-    await ftp.removeFile("payload_remove.jpg")
-    await ftp.createDir("deleteme")
-    await ftp.removeDir("deleteme")
-    echo("Finished")
-
-  waitFor main(ftp)
